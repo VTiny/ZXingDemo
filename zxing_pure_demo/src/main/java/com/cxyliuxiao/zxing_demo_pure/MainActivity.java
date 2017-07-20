73f4a608a3ae8dcabb3dead54dfdc117ce512138
liuxiao <liuxiao@netease-liuxiao-mac-mini.local>
2017-07-20 20:37:16 +0800

    添加pure精简library和demo

--- zxing_pure_demo/src/main/java/com/cxyliuxiao/zxing_demo_pure/MainActivity.java
+++ zxing_pure_demo/src/main/java/com/cxyliuxiao/zxing_demo_pure/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cxyliuxiao.zxing_demo_pure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.client.android.SimpleScanActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SimpleScanActivity.class));
     }
 }
